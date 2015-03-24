9198402cda25262a917d8d54c0c42af4d2a22eb9
roobew <roobew@netbook>
2015-03-24 11:54:48 +0100

    daupoivo

--- Phi/src/server/Physics.java
+++ Phi/src/server/Physics.java
@@ -6,13 +6,30 @@ public class Physics {
 	
 	private ArrayList <GameObject> world;
 	
+	
 	public boolean add(GameObject obj){
 		return world.add(obj);
 	}
 	
 	
-	public void update(long delta){
+	public void update(double delta){
 		for(GameObject obj: world){
+			if( obj instanceof Player){
+				Player p = (Player) obj;
+				if(p.key.RIGHT==true){
+					p.incX((int) (0.004*delta));
+				}else if(p.key.LEFT==true){
+					p.incX((int) (-0.004*delta));
+				}
+				if(p.key.UP==true){
+					p.incY((int) (-0.004*delta));
+				}
+			}
+		}
+				
+				
+					
+			}
 			
 		}
 		
